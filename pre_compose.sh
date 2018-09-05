312b32de06e550e02c8f2114a73654715203a09c
bhefny-dievision <hefny@dievision.de>
2018-09-05 18:50:03 +0200

    dev ready

--- pre_compose.sh
+++ pre_compose.sh
@@ -2,10 +2,10 @@
 DIR=$(dirname -- "$0")
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 if ! [ -z "$TALENT_PRODUCTION" ]; then # PRODUCTION
-  ln -sf ./docker-compose_prd.yml docker-compose.yml
+  cp -f docker-compose_prd.yml docker-compose.yml
   cp -f $DIR/Dockerfile.prd $DIR/talententdecker/Dockerfile.dev
 else # DEVELOPMENT
-  ln -sf ./docker-compose_dev.yml docker-compose.yml
+  cp -f docker-compose_dev.yml docker-compose.yml
   cp -f $DIR/Dockerfile.dev $DIR/talententdecker/Dockerfile.dev
 fi
 
