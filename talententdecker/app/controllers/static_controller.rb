0a3ca0cf8d59092ba7769f5c0f2d2e89485f6c83
bhefny-dievision <hefny@dievision.de>
2018-09-06 15:12:51 +0200

    some translations

--- talententdecker/app/controllers/static_controller.rb
+++ talententdecker/app/controllers/static_controller.rb
@@ -1,6 +1,11 @@
 class StaticController < ApplicationController
 
   def index
+    @team = [
+      {name: "Nizar Hegazy", title: "Recruitment Specialist", image: "nhegazy.jpg", facebook: "nhegazy", linkedin: "nhegazy"},
+      {name: "Mohamed Moursy", title: "Business Development", image: "mmoursy.jpg", facebook: "mmoursy", linkedin: "mmoursy"},
+      {name: "Bassem Hefny", title: "Technology Director", image: "bhefny.jpg", facebook: "bhefny", linkedin: "bhefny"},
+    ]
     @year_footer = GlobalSetting.find_by(key: 'year_footer').try(:value)
   end
 
