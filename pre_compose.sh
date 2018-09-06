d4a83bad24e70d83b103b1308153eb108293bdf5
bhefny-dievision <hefny@dievision.de>
2018-09-06 15:14:31 +0200

    linux differences

--- pre_compose.sh
+++ pre_compose.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 DIR=$(dirname -- "$0")
-DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+DIR="$( cd "$(dirname -- "$0")" ; pwd -P )"
 if ! [ -z "$TALENT_PRODUCTION" ]; then # PRODUCTION
   cp -f docker-compose_prd.yml docker-compose.yml
   cp -f $DIR/Dockerfile.prd $DIR/talententdecker/Dockerfile.dev
