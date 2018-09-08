29ca7d72ebd2d7c864d3256ab252b9dfca2fdf76
bhefny-dievision <hefny@dievision.de>
2018-09-08 15:12:02 +0200

    production edition

--- pre_compose.sh
+++ pre_compose.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-DIR=$(dirname -- "$0")
 DIR="$( cd "$(dirname -- "$0")" ; pwd -P )"
 if ! [ -z "$TALENT_PRODUCTION" ]; then # PRODUCTION
+  echo "-----PRODUTION-----"
   cp -f docker-compose_prd.yml docker-compose.yml
   cp -f $DIR/Dockerfile.prd $DIR/talententdecker/Dockerfile.dev
 else # DEVELOPMENT
+  echo "-----DEVELOPMENT-----"
   cp -f docker-compose_dev.yml docker-compose.yml
   cp -f $DIR/Dockerfile.dev $DIR/talententdecker/Dockerfile.dev
 fi
-
-
