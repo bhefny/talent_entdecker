823cdb11dbccc7d7d6e5f311cfd48216017510ad
bhefny-dievision <hefny@dievision.de>
2018-09-11 20:47:41 +0200

    adding SendGrid moving more to language file

--- talententdecker/app/controllers/static_controller.rb
+++ talententdecker/app/controllers/static_controller.rb
@@ -6,6 +6,7 @@ class StaticController < ApplicationController
       {name: "Mohamed Moursy", title: "Business Development", image: "mmoursy.jpg", facebook: "mmoursy", linkedin: "mmoursy"},
       {name: "Bassem Hefny", title: "Technology Director", image: "bhefny.jpg", facebook: "bhefny", linkedin: "bhefny"},
     ]
+    @technologies = %w(ruby php js docker)
     @year_footer = GlobalSetting.find_by(key: 'year_footer').try(:value)
   end
 
